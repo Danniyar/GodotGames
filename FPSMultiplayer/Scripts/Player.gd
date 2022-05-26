c0ee43460e0fd8f6d04b758c1e0204ad3eb1b78c
Blocky7 <61798065+Blocky7@users.noreply.github.com>
2022-05-26 14:30:33 +0600

    Add files via upload

--- FPSMultiplayer/Scripts/Player.gd
+++ FPSMultiplayer/Scripts/Player.gd
@@ -103,7 +103,7 @@ func _physics_process(delta):
 		if(Input.is_action_just_pressed("MouseOnOrOff")):
 			mouseOn = !mouseOn
 			if(mouseOn):
-				Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			else:
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		var move_vec = Vector3()
